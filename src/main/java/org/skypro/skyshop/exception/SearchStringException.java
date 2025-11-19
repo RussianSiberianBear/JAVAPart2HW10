c809be1635f1427b62ab84af3d12e163de820527
Alex <alexbaturin@list.ru>
2025-11-19 11:26:09 +0800

    HW13 PR3

--- src/main/java/org/skypro/skyshop/exception/SearchStringException.java
+++ src/main/java/org/skypro/skyshop/exception/SearchStringException.java
@@ -1,9 +1,9 @@
 package org.skypro.skyshop.exception;
 
-import org.skypro.skyshop.error.ShopError;
-
 public class SearchStringException extends RuntimeException {
     public SearchStringException(String message) {
-        ShopError shopError = new ShopError(400, message);
+        super(message);
+   //     ShopError shopError = new ShopError(400, message);
+
     }
 }
